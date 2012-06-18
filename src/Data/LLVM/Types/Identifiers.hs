1e0fda37f22870ba89c72028856edb8e1f1851e8
Tristan Ravitch <travitch@cs.wisc.edu>
2012-06-18 14:41:24 -0500

    Store identifiers using SmallString instead of Text
    Each SmallString has much lower overhead than a Text.  Additionally,
    SmallString stores its contents in UTF8 instead of UTF16, which will
    save a lot of space for LLVM where all identifiers are currently
    ASCII.
    
    It is still future-proof (and still more compact for unicode text than
    Text).

--- src/Data/LLVM/Types/Identifiers.hs
+++ src/Data/LLVM/Types/Identifiers.hs
@@ -14,25 +14,26 @@ module Data.LLVM.Types.Identifiers (
 
 import Control.DeepSeq
 import Data.Hashable
+import Data.SmallString
 import Data.Text ( Text, unpack, pack )
 
-data Identifier = LocalIdentifier { _identifierContent :: !Text
+data Identifier = LocalIdentifier { _identifierContent :: !SmallString
                                   , _identifierHash :: !Int
                                   }
                 | AnonymousLocalIdentifier { _identifierNumber :: !Int }
-                | GlobalIdentifier { _identifierContent :: !Text
+                | GlobalIdentifier { _identifierContent :: !SmallString
                                    , _identifierHash :: !Int
                                    }
-                | MetaIdentifier { _identifierContent :: !Text
+                | MetaIdentifier { _identifierContent :: !SmallString
                                  , _identifierHash :: !Int
                                  }
                   deriving (Eq, Ord)
 
 instance Show Identifier where
-  show LocalIdentifier { _identifierContent = t } = '%' : unpack t
+  show LocalIdentifier { _identifierContent = t } = '%' : toString t
   show AnonymousLocalIdentifier { _identifierNumber = n } = '%' : show n
-  show GlobalIdentifier { _identifierContent = t } = '@' : unpack t
-  show MetaIdentifier { _identifierContent = t } = '!' : unpack t
+  show GlobalIdentifier { _identifierContent = t } = '@' : toString t
+  show MetaIdentifier { _identifierContent = t } = '!' : toString t
 
 instance Hashable Identifier where
   hash (AnonymousLocalIdentifier n) = hash n
@@ -47,19 +48,19 @@ makeAnonymousLocal = AnonymousLocalIdentifier
 
 makeLocalIdentifier :: Text -> Identifier
 makeLocalIdentifier t =
-  LocalIdentifier { _identifierContent = t
+  LocalIdentifier { _identifierContent = fromText t
                   , _identifierHash = hash t
                   }
 
 makeGlobalIdentifier :: Text -> Identifier
 makeGlobalIdentifier t =
-  GlobalIdentifier { _identifierContent = t
+  GlobalIdentifier { _identifierContent = fromText t
                    , _identifierHash = hash t
                    }
 
 makeMetaIdentifier :: Text -> Identifier
 makeMetaIdentifier t =
-  MetaIdentifier { _identifierContent = t
+  MetaIdentifier { _identifierContent = fromText t
                  , _identifierHash = hash t
                  }
 
@@ -69,7 +70,7 @@ identifierAsString i = unpack (identifierContent i)
 
 identifierContent :: Identifier -> Text
 identifierContent (AnonymousLocalIdentifier n) = pack (show n)
-identifierContent i = _identifierContent i
+identifierContent i = toText $ _identifierContent i
 
 isAnonymousIdentifier :: Identifier -> Bool
 isAnonymousIdentifier AnonymousLocalIdentifier {} = True
